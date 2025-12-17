c1ea43c28a5b900d20a0c56ee3465f444e19d9fc
Banaanas <cyril.developpeur@gmail.com>
2025-12-17 13:52:53 +0100

    Replace inline logo and text in Header with `FlowerMetalIcon` and `FlowerMetalBanner` components

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,17 +1,20 @@
-import Image from "next/image";
-
 import { AboutDialogButton } from "@/components/layout/header/about-dialog-button";
+import { FlowerMetalBanner } from "@/components/layout/header/flower-metal-banner";
 import GoogleSheetsLink from "@/components/layout/header/google-sheets-link";
 import { ToggleThemeButton } from "@/components/layout/theme-button";
-import flowerMetalLogoSVG from "@/public/assets/flower-metal.png";
+import { FlowerMetalIcon } from "@/components/shared/icons/flower-metal-icon";
 
 export const Header = () => {
   return (
     <header className="border-b border-border bg-card/80 backdrop-blur-sm top-0 z-50">
       <div className="mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center gap-x-3">
-          <HeaderImage />
-          <HeaderText />
+          <FlowerMetalIcon />
+
+          <div className="flex flex-col">
+            <FlowerMetalBanner />
+            <HeaderText />
+          </div>
         </div>
         <div className="flex items-center gap-x-3">
           <AboutDialogButton />
@@ -25,22 +28,8 @@ export const Header = () => {
 
 const HeaderText = () => {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-foreground">Flower metal</h1>
-      <p className="text-xs text-muted-foreground">
-        Because we do like clean metal.
-      </p>
-    </div>
-  );
-};
-
-const HeaderImage = () => {
-  return (
-    <Image
-      src={flowerMetalLogoSVG}
-      alt="FlowerMetalLogo"
-      width={32}
-      height={32}
-    />
+    <p className="text-xs text-muted-foreground">
+      Because we do like clean metal.
+    </p>
   );
 };
