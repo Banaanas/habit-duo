6c8913cd50b2032b2fb62eac39bc8b5a298172f1
Banaanas <cyril.developpeur@gmail.com>
2025-12-19 20:06:42 +0100

    Add caching mechanism to `fetchGoogleSheets` using `next/cache` utilities

--- src/lib/api/fetch-sheets.ts
+++ src/lib/api/fetch-sheets.ts
@@ -1,8 +1,13 @@
+import { cacheLife } from "next/cache";
+
 import { SHEETS, SheetKey } from "@/data/google-sheets.config";
 import { fetchSingleSheet } from "@/lib/api/fetch-single-sheet";
 import type { GoogleSheetItem } from "@/types/google-sheet";
 
 export const fetchGoogleSheets = async (): Promise<GoogleSheetItem[]> => {
+  "use cache";
+  cacheLife("hours");
+
   const allSheets = Object.keys(SHEETS) as SheetKey[];
 
   const results = await Promise.all(allSheets.map(fetchSingleSheet));
