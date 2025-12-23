2f65164afb024c4f2894daa2e64ec5ef39683536
Banaanas <cyril.developpeur@gmail.com>
2025-12-23 22:14:24 +0100

    feat: improve header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,22 +1,33 @@
+import { Users } from "lucide-react";
 import Link from "next/link";
 
-import { HabitDuo } from "@/components/layout/header/habit-duo";
 import { ToggleThemeButton } from "@/components/layout/theme-button";
-import { HabitDuoIcon } from "@/components/shared/icons/habit-duo-icon";
 import { appNavLinks } from "@/data/app-data";
 
 export const Header = () => {
   return (
     <header className="border-b border-border bg-muted backdrop-blur-sm top-0 z-50">
       <div className="mx-auto px-4 py-4 flex items-center justify-between flex-wrap">
-        <Link
-          href={appNavLinks.home.href}
-          className="flex items-center gap-x-3"
-          style={{ height: "clamp(32px, 10vw, 64px)" }}
-        >
-          <HabitDuoIcon />
-          <HabitDuo />
-        </Link>
+        <div className="flex items-center gap-x-2">
+          <Link
+            href={appNavLinks.home.href}
+            className="flex items-center gap-x-3"
+            style={{ height: "clamp(32px, 10vw, 64px)" }}
+          >
+            <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-primary to-chart-1 rounded-xl">
+              <Users className="w-6 h-6 text-primary-foreground" />
+            </div>{" "}
+          </Link>
+
+          <div>
+            <div className="text-2xl font-bold tracking-tight text-foreground">
+              HabitDuo
+            </div>
+            <div className="text-sm text-muted-foreground">
+              Build habits together
+            </div>
+          </div>
+        </div>
         <div className="flex items-center gap-x-3">
           <ToggleThemeButton />
         </div>
