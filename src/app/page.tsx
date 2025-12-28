ad01abd053f4dd56bcaafbf3ea22fb8b77e74e27
Banaanas <cyril.developpeur@gmail.com>
2025-12-28 17:34:51 +0100

    refactor: remove useless skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { QUERY_PARAMS } from "@/lib/query-params";
 const HomePage = ({ searchParams }: HomePageProps) => {
   return (
     <div className="flex w-full justify-center items-center flex-col">
-      <DashboardSkeleton />
       <Suspense fallback={<DashboardSkeleton />}>
         <Dashboard searchParams={searchParams} />
       </Suspense>
